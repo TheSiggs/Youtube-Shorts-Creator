6ac7882d9072554edf64d07809b2312e6b0fe304
Sam Siggs <samsiggs00@gmail.com>
2022-11-30 23:17:44 +1300

    Added video uploading using youtube's api

--- index.php
+++ index.php
@@ -1,33 +1,48 @@
 <?php
 
-require 'vendor/autoload.php';
+require_once realpath(__DIR__ . '/vendor/autoload.php');
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+
 require_once 'curl_wrapper.php';
 use JSON2Video\Movie;
 use JSON2Video\Scene;
 
+/**
+ * MOVIE CREATION
+ */
+/*
 // Create a new movie
 $movie = new Movie;
 
 // Set your API key
 // Get your free API key at https://json2video.com
-$movie->setAPIKey('04QLiatYSb37jpsOwDZs56RjEPqE2Pfy3wcxWwab');
+$movie->setAPIKey($_ENV['JSON2VIDEO_API_KEY']);
+
+// Get a random video from the videos files
+$videos = json_decode(file_get_contents("videos.json"));
+$choice = array_rand($videos);
+$chosenVideo = ($videos[$choice]);
 
 // Set movie quality: low, medium, high
 $movie->quality = 'high';
-$movie->draft = true;
-$movie->width = 607;
-$movie->height = 1080;
+$movie->draft = $_ENV['ENV'] === 'dev';
+$movie->width = $chosenVideo->width;
+$movie->height = $chosenVideo->height;
 
 // Adds a video element directly to the movie
 $movie->addElement([
     'type' => 'video',
-    'src' => 'https://drive.google.com/file/d/106GlzUvpHB-iZQhCUMKFQlxEkW7kmAQW/view?usp=sharing',
-    'x' => -657,
+    'src' => $chosenVideo->video_link,
+    'x' => $chosenVideo->x,
+    'z-index' => -1,
 ]);
 
 // Fetch posts from the reddit api
-$resp = runCurl('https://www.reddit.com/r/Showerthoughts/.json');
-$posts = array_slice($resp->data->children, 0, 7, true);
+$resp = runCurl($_ENV['SUBREDDIT']);
+$posts = array_slice($resp->data->children, 0, 5, true);
 // Loop through the post and create scenes for each post
 foreach ($posts as $post) {
     $text = $post->data->title; // Text to be used for each scene
@@ -38,21 +53,31 @@ foreach ($posts as $post) {
         'text' => $text,
         'start' => 1
     ]);
+    $scene->background_color = 'transparent';
 
     // Add a block with the post text on the screen
+//    $scene->addElement([
+//        'type' => 'component',
+//        'component' => 'basic/000',
+//        "background-color" => "transparent",
+//        'settings' => [
+//            'card' => [
+//                'background' => 'white',
+//                'border-radius' => '10px'
+//            ],
+//            'headline' => [],
+//            'body' => [
+//                'text' => $text,
+//                'color' => 'black'
+//            ]
+//        ]
+//    ]);
     $scene->addElement([
-        'type' => 'component',
-        'component' => 'basic/000',
+        'type' => 'text',
+        'text' => $text,
         'settings' => [
-            'card' => [
-                'background' => 'white',
-                'border-radius' => '10px'
-            ],
-            'headline' => [],
-            'body' => [
-                'text' => $text,
-                'color' => 'black'
-            ]
+            'font-size' => '5vh',
+            'text-shadow' => '2px 2px rgba(0,0,0, 1)',
         ]
     ]);
     // Add the scene to the movie
@@ -73,3 +98,58 @@ if (file_put_contents($file_name, file_get_contents($url))) {
 } else {
     echo "File downloading failed." . PHP_EOL;
 }
+*/
+/**
+ * VIDEO UPLOAD
+ */
+
+$client = new Google_Client();
+$client->setApplicationName('API code samples');
+$client->setScopes([
+    'https://www.googleapis.com/auth/youtube.upload',
+]);
+$client->setAuthConfig('secret.json');
+$client->setAccessType('online');
+
+// Request authorization from the user.
+// TODO: FIGURE OUT HOW TO AUTOMATE THIS
+$authUrl = $client->createAuthUrl();
+printf("Open this link in your browser:\n%s\n", $authUrl);
+print('Enter verification code: ');
+$authCode = trim(fgets(STDIN));
+
+// Exchange authorization code for an access token.
+$accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+$client->setAccessToken($accessToken);
+
+// Define service object for making API requests.
+$service = new Google_Service_YouTube($client);
+
+// Define the $video object, which will be uploaded as the request body.
+$video = new Google_Service_YouTube_Video();
+
+// Add 'snippet' object to the $video object.
+$videoSnippet = new Google_Service_YouTube_VideoSnippet();
+$videoSnippet->setCategoryId('22');
+//$videoSnippet->setDescription('Description of uploaded video.');
+// TODO: SET THIS DYNAMICALLY
+$videoSnippet->setTitle('Shower Thoughts 3');
+$videoSnippet->setTags(explode(',', $_ENV['TAGS']));
+$video->setSnippet($videoSnippet);
+
+// Add 'status' object to the $video object.
+$videoStatus = new Google_Service_YouTube_VideoStatus();
+$videoStatus->setPrivacyStatus('public');
+$video->setStatus($videoStatus);
+
+$response = $service->videos->insert(
+    'snippet,status',
+    $video,
+    array(
+        // TODO: SET THIS DYNAMICALLY
+        'data' => file_get_contents("2022-11-30-27751.mp4"),
+        'mimeType' => 'application/octet-stream',
+        'uploadType' => 'multipart'
+    )
+);
+print_r($response);

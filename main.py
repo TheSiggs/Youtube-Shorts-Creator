e62c05d5340d93a861ed0f0e562f657a7cb461e7
Sam Siggs <samsiggs00@gmail.com>
2023-02-18 22:02:47 +1300

    moved automation to selenium

--- main.py
+++ main.py
@@ -1,104 +1,78 @@
-import pyautogui as gui
-import time
 import subprocess
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+from time import sleep
 
 
-def click(x, y): gui.click(x, y)
+def upload_youtube():
+    driver.switch_to.new_window('tab')
+    driver.get('https://studio.youtube.com/channel/UCkrrIuQTIUcvVobIl_0ofTg')
+    sleep(10)
+    driver.find_element(By.XPATH, '//*[@id="create-icon"]').click()
+    sleep(0.5)
+    driver.find_element(By.XPATH, '//*[@id="text-item-0"]').click()
+    sleep(1)
+    chooseFileYT = driver.find_element(By.XPATH, '//*[@name="Filedata"]')
+    chooseFileYT.send_keys(
+        "S:\\Projects\\SeleniumTest\\We shortened Television to 'TV', but Telephone to 'phone'. #shorts.mp4")
+    sleep(10)
+    driver.find_element(By.XPATH, '//*[@id="next-button"]').click()
+    sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="next-button"]').click()
+    sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="next-button"]').click()
+    sleep(1)
+    driver.find_element(By.XPATH, '//*[@name="PUBLIC"]').click()
+    sleep(0.1)
+    driver.find_element(By.XPATH, '//*[@id="done-button"]').click()
+    sleep(0.5)
 
 
-def wait(t): time.sleep(t)
+def upload_ig():
+    driver.switch_to.new_window('tab')
+    driver.get('https://www.instagram.com/redditssaid/')
+    sleep(3)
+    driver.find_element(By.PARTIAL_LINK_TEXT, 'Create').click()
+    sleep(2)
+    inputs = driver.find_elements(By.XPATH, '//input[@type="file"]')
+    chooseFile = inputs[-1]
+    chooseFile.send_keys(
+        "S:\\Projects\\SeleniumTest\\We shortened Television to 'TV', but Telephone to 'phone'. #shorts.mp4")
+    sleep(1)
+    buttons = driver.find_elements(By.XPATH, '//button')
+    next((x for x in buttons if x.text == 'Next'), None).click()
+    sleep(1)
+    buttons = driver.find_elements(By.XPATH, '//button')
+    next((x for x in buttons if x.text == 'Next'), None).click()
+    sleep(1)
+    buttons = driver.find_elements(By.XPATH, '//button')
+    next((x for x in buttons if x.text == 'Share'), None).click()
 
 
-def open_chrome():
-    gui.keyDown('command')
-    gui.keyDown('space')
-    gui.keyUp('space')
-    gui.keyUp('command')
-    wait(0.5)
-    gui.write('Google Chrome')
-    gui.press('enter')
+def upload_tiktok():
+    driver.switch_to.new_window('tab')
+    driver.get('https://www.tiktok.com/upload/')
+    sleep(5)
+    iframe = driver.find_element(By.XPATH, '//iframe')
+    driver.switch_to.frame(iframe)
+    chooseFile = driver.find_element(By.XPATH, '//input[@type="file"]')
+    chooseFile.send_keys("S:\\Projects\\SeleniumTest\\We shortened Television to 'TV', but Telephone to 'phone'. #shorts.mp4")
+    sleep(20)
+    driver.find_element(By.XPATH, "//button/div/div[text()='Post']").click()
 
-
-# Youtube
-def upload_to_youtube():
-    gui.hotkey('command', 't')
-    wait(1)
-    gui.write('studio.youtube.com')
-    gui.press('enter')
-    wait(20)
-    gui.press('tab', presses=36)
-    wait(1)
-    gui.press('enter')
-    wait(1)
-    gui.press('enter')
-    wait(1)
-    gui.press('tab', presses=3)
-    wait(1)
-    gui.press('enter')
-    wait(1)
-    gui.press('down')
-    wait(1)
-    gui.press('enter')
-    wait(5)
-    click(1194, 905)
-    wait(1)
-    click(1194, 905)
-    wait(1)
-    click(1194, 905)
-    wait(1)
-    gui.press('down', presses=2)
-    click(1194, 905)
-
-
-def upload_to_ig():
-    gui.hotkey('command', 't')
-    wait(1)
-    gui.write('www.instagram.com')
-    gui.press('enter')
-    wait(20)
-    gui.press('tab', presses=8)
-    gui.press('enter')
-    wait(1)
-    gui.press('tab', presses=1)
-    gui.press('enter')
-    wait(1)
-    gui.press('down')
-    gui.press('enter')
-    wait(5)
-    gui.press('tab', presses=2)
-    gui.press('enter')
-    wait(1)
-    gui.press('tab', presses=2)
-    gui.press('enter')
-    wait(1)
-    gui.press('tab', presses=2)
-    gui.press('enter')
-
-
-# TikTok
-def upload_to_tiktok():
-    gui.hotkey('command', 't')
-    wait(1)
-    gui.write('https://www.tiktok.com/upload')
-    gui.press('enter')
-    wait(20)
-    gui.press('tab', presses=5)
-    gui.press('enter')
-    wait(1)
-    gui.press('down')
-    gui.press('enter')
-    wait(40)
-    gui.press('tab', presses=8)
-    gui.press('enter')
 
 
 # Run PHP Script
 out = subprocess.call("docker-compose run php81-service php index.php", shell=True)
-wait(1)
 if out == 0:
-    open_chrome()
-    upload_to_youtube()
-    upload_to_ig()
-    upload_to_tiktok()
+    options = uc.ChromeOptions()
+    options.add_argument("--ignore-certificate-error")
+    options.add_argument("--ignore-ssl-errors")
+    # e.g. Chrome path in Mac =/Users/x/Library/xx/Chrome/Default/
+    options.add_argument("--user-data-dir=C:\\Users\\samsi\\AppData\\Local\\Google\\Chrome\\User Data\\Default")
+    driver = uc.Chrome(options=options)
+    upload_youtube()
+    upload_ig()
+    upload_tiktok()
 else:
     print("PHP script failed :(")

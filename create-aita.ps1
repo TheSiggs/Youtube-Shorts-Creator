58e6e8527643883939889f71b376f25aea9560c3
Sam Siggs <samsiggs00@gmail.com>
2023-03-14 15:47:06 +1300

    Set up templates for schedules to run

---
+++ create-aita.ps1
@@ -0,0 +1,3 @@
+git pull
+echo '[{"name":"Am I The Asshole","link":"https://www.reddit.com/r/AmItheAsshole/top/.json","posts":1,"include":true,"language":"en","youtube_hashtags":"#shorts","instagram_tags":"#shorts","tiktok_tags":"#shorts","voice":"en-AU-DarrenNeural","showTitle":true,"showBody":true}]' > subreddits.json
+python main.py
---
+++ create-shower-thoughts.ps1
@@ -0,0 +1,3 @@
+git pull
+echo '[{"name":"Shower Thoughts","link":"https://www.reddit.com/r/Showerthoughts/hot/.json","posts":7,"include":true,"language":"en","youtube_hashtags":"#shorts","instagram_tags":"#shorts","tiktok_tags":"#shorts","voice":"en-AU-DarrenNeural","showTitle":true,"showBody":false}]' > subreddits.json
+python main.py
